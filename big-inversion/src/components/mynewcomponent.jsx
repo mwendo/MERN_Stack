b060c53ef9c8bf4fdaf81750934fc56bcf98a448
Micah Wendorf <wendorfmicah0@gmail.com>
2021-05-06 14:21:08 -0500

    initial commit

---
+++ big-inversion/src/components/mynewcomponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Personcard = props => {
+    return(
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair color: {props.hairColor}</p>
+        </div>
+    );
+}
+
+export default Personcard;
